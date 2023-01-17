62e59088d0c7811d8ec486f73da6a110b1da79ee
Paul Bailey <allthebaileys@yahoo.co.uk>
2023-01-17 14:01:46 +0000

    section 1: React JSX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import * as React from 'react';
 
-const title = 'React';
+const welcome = {
+  greeting: 'hello',
+  title: 'Paul',
+};
+
+function getMsg(MsgObj) {
+  let Msg;
+  Msg = MsgObj.greeting + ' to ' + MsgObj.title
+  return Msg;
+}
 
 function App() {
+  let titleMsg;
+  titleMsg = getMsg(welcome);
+
   return (
     <div>
-      <h1>Hello {title}</h1>
-      <h2>What a pickle</h2>
+      <h1>{titleMsg}</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+      <h1>The final end</h1>
     </div>
   );
 }
